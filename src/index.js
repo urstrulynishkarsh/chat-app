9f6e33c70f40986b2f41872628ab65681244f535
urstrulynishkarsh <206301010@gkv.ac.in>
2023-07-28 07:11:17 +0530

    finall commit

--- src/index.js
+++ src/index.js
@@ -20,15 +20,15 @@ io.on('connection', (socket) => {
 
     socket.on('join', (options, callback) => {
         const { error, user } = addUser({ id: socket.id, ...options })
-
+        
         if (error) {
             return callback(error)
         }
 
         socket.join(user.room)
 
-        socket.emit('message', generateMessage('Admin', 'Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+        socket.emit('message', generateMessage(`${user.username}`, 'Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username}`, `${user.username} has joined!`))
         io.to(user.room).emit('roomData', {
             room: user.room,
             users: getUsersInRoom(user.room)
@@ -59,7 +59,7 @@ io.on('connection', (socket) => {
         const user = removeUser(socket.id)
 
         if (user) {
-            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+            io.to(user.room).emit('message', generateMessage(`${user.username}`, `${user.username} has left!`))
             io.to(user.room).emit('roomData', {
                 room: user.room,
                 users: getUsersInRoom(user.room)
